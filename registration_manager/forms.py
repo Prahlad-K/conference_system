91439d01e8a5fa8b3f480980fb7ec84ef65b7a4b
Prahlad <prahladsharmak99@gmail.com>
2018-10-23 23:18:52 +0530

    Payments completed for evaluation, basic version

--- registration_manager/forms.py
+++ registration_manager/forms.py
@@ -5,7 +5,6 @@ from django.utils.translation import ugettext_lazy as _
 class collectPaymentForm(forms.Form):
     credit_card_no = forms.CharField(help_text = "Enter your credit card no.",max_length=20)
     amount = forms.IntegerField()
-    user_details = forms.CharField(max_length=200)
     payment_date = forms.DateTimeField()
     
         
--- registration_manager/urls.py
+++ registration_manager/urls.py
@@ -5,9 +5,10 @@ app_name = 'registration_manager'
 
 urlpatterns = [
 
+    path('', views.display, name='display'),
     path('payment/', views.index, name='index'),
-    #path('payment/<int:payment_id>/status', views.status, name='status'),
-    #path('payment/<int:payment_id>/wait', views.wait, name='wait'),
-    #path('payment/<int:payment_id>/approve', views.approve, name='approve'),
-    #path('payment/<int:payment_id>/complete', views.complete, name='complete'),
+    path('payment/<int:payment_id>/status', views.status, name='status'),
+    path('payment/<int:payment_id>/wait', views.wait, name='wait'),
+    path('payment/<int:payment_id>/approve', views.approve, name='approve'),
+    path('payment/<int:payment_id>/complete', views.complete, name='complete'),
 ]

77cb6e472bf90d5394cefd50fbf148d6bbdb583b
Prahlad <prahladsharmak99@gmail.com>
2018-11-17 21:19:56 +0530

    Improving the author, reviewer, track chair backends

--- track_chair/views.py
+++ track_chair/views.py
@@ -11,12 +11,28 @@ from conference_manager.models import Track
 def index(request):
     tracks = Track.objects.filter(track_chair=request.user)
 
-    return render(request, 'track_chair/index.html', {'tracks':tracks})
+    total_tracks = len(tracks)
+
+    tracks_under_permission = Track.objects.filter(reviewer = request.user, permission_requested= True)
+    no_tracks_under_permission = len(tracks_under_permission)
+
+    approved_tracks = Track.objects.filter(author = request.user, track_approved= True)
+    no_approved_tracks = len(approved_tracks)
+
+    return render(request, 'track_chair/index.html', {'tracks':tracks, 'total_tracks':total_tracks, 'no_tracks_under_permission':no_tracks_under_permission, 'no_approved_tracks':no_approved_tracks})
 
 def approve(request, track_id):
     track = Track.objects.get(pk = track_id)
     track.report_submitted = False
-    track.report_approved = True
+    track.permission_requested = True
     track.save()
     tracks = Track.objects.filter(track_chair=request.user)
-    return render(request, 'track_chair/index.html', {'tracks':tracks})
+    total_tracks = len(tracks)
+
+    tracks_under_permission = Track.objects.filter(reviewer = request.user, permission_requested= True)
+    no_tracks_under_permission = len(tracks_under_permission)
+
+    approved_tracks = Track.objects.filter(author = request.user, track_approved= True)
+    no_approved_tracks = len(approved_tracks)
+
+    return render(request, 'track_chair/index.html', {'tracks':tracks, 'total_tracks':total_tracks, 'no_tracks_under_permission':no_tracks_under_permission, 'no_approved_tracks':no_approved_tracks})
